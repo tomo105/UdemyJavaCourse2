225ce183857af7d8be28f1f0b4552ae4497ef68c
Tomek <tomo105@wp.pl>
2019-10-02 20:27:17 +0200

    ending third section

--- src/ex1/Main.java
+++ src/ex1/Main.java
@@ -51,16 +51,61 @@ public class Main {
         //convert pounds to kilos
         double pounds = 200d;
         double kilos = pounds * 0.45359237d;
-        System.out.println(pounds+ " pounds = " +kilos +" kilos" );
+        System.out.println(pounds + " pounds = " + kilos + " kilos");
         double Pi = 3.1415927d;
-        double anotherNumber =  3_000_000.4_567_890d;  //zero was ignored
-        System.out.println(Pi +" "+ anotherNumber);
+        double anotherNumber = 3_000_000.4_567_890d;  //zero was ignored
+        System.out.println(Pi + " " + anotherNumber);
 
         char myChar = 'D'; //only one character
-         char myCharChinese = '\u0194';
+        char myCharChinese = '\u0194';
         System.out.println(myCharChinese);
         boolean aa = true;
         boolean b = false;
+        String myString = "zapierdalaj!";
+        System.out.println(myString);
+        String anotherString = "inny string +\u0007 " + myString;
+        System.out.println(anotherString);
+        int operator = 45 + 5;
+        boolean trueStatement = true;
+
+        while (operator >= 40) {
+            System.out.println((Integer.parseInt("100") + operator));
+            operator--;
+        }
+
+        int topScore = 102;
+        if (topScore != 100 || topScore > 40)
+            System.out.println("you fucking win!!");
+
+        //challenge
+        int something = 50;
+        if (something == 50) {
+            System.out.println("no siema");
+        }
+
+        boolean isCar = false;
+        int isDefinatelyCar = isCar ? 3 :  7;
+        System.out.println(isDefinatelyCar);
+
+        //challenge
+        double twenty = 20.00d;
+        double eighty = 80.00d;
+        double summary = (twenty + eighty) * 100.00d;
+        double remainder  = summary % 40.0d;
+        boolean ifNotZero = (remainder == 0 ) ? true : false;// or remainder == 0
+        System.out.println(ifNotZero);
+        if(! ifNotZero)
+        {
+            System.out.println("Got some  remainder");
+        }
 
     }
 }
+
+
+
+
+
+
+
+
