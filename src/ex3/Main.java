dd061ec8faa71b3245197f8d4674215a999e2981
Tomek <tomo105@wp.pl>
2019-10-23 14:03:31 +0200

    add some excersise Moto !!

--- src/ex3/Main.java
+++ src/ex3/Main.java
@@ -1,7 +1,26 @@
 package ex3;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj liczbe");
+        int liczba = scanner.nextInt();
+        System.out.println(liczba);
+
+        boolean gameOver = true;
+        int score = 18000;
+        int levelCompleted = 8;
+        int bonus = 800;
+
+        if(gameOver)
+        {
+
+        }
+
 
     }
+
 }
