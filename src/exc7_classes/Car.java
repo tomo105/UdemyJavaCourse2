f28ec8c64c5ec5b79ebc30ea0bac0c67a561a7fd
Tomek <tomo105@wp.pl>
2020-02-28 16:51:01 +0100

    remind some classes staff

---
+++ src/exc7_classes/Car.java
@@ -0,0 +1,15 @@
+package exc7_classes;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String color;
+
+    public Car(int doors, int wheels, String model, String color) {
+        this.doors = doors;
+        this.wheels = wheels;
+        this.model = model;
+        this.color = color;
+    }
+}
---
+++ src/exc7_classes/Main.java
@@ -0,0 +1,8 @@
+package exc7_classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Car asd = new Car(5,4,"Lambo", "red");
+
+    }
+}
