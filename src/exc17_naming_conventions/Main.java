eb99e5ccc5744d799079b5d07c2818411bccd82e
Tomek <tomo105@wp.pl>
2020-04-09 01:09:54 +0200

    make .jar library and test it by adding it to project

--- src/exc17_naming_conventions/Main.java
+++ src/exc17_naming_conventions/Main.java
@@ -1,9 +1,25 @@
 package exc17_naming_conventions;
 
+import com.exmaple.mypackage.Series;
+
 public class Main {
     public static void main(String[] args) {
 
         MyWindow myWindow = new MyWindow("Java");
         myWindow.setVisible(true);
+
+        // i have made a .jar file from class Series with static method nSum(int n), factorial(int n) and fibonacci(int n)
+        // added jar file to my packages
+        // test if this works :
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.nSum(i));
+        }
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.factorial(i));
+        }
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.fibonacci(i));
+        }
+
     }
 }
